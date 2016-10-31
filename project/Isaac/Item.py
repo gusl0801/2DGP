a604baeb9c120118a1b858393c057b3b097837e3
gusl0801 <gusl0801@daum.net>
2016-10-31 20:20:28 +0900

    Add Item class's subclass Key and Heart

--- project/Isaac/Item.py
+++ project/Isaac/Item.py
@@ -6,14 +6,39 @@ class ItemState:
     #소멸
 
 class Item:
-    def __init__(self,x,y):
+    def __init__(self,x,y, path):
         self.x, self.y = x,y
-        self.renderer = Renderer.Renderer()
+        self.renderer = Renderer.Renderer(path)
         pass
 
     def draw(self):
         self.renderer.update()
         pass
 
-    def update(self):
+    def update(self, frame):
+        self.renderer.update(frame)
         pass
+
+class Heart(Item):
+    def __init__(self,x,y):
+        Item.__init__(self,x,y, "")
+
+    def draw(self):
+        Item.draw(self)
+
+    def update(self):
+        Item.update(self, 0)
+
+
+class Key(Item):
+    def __init__(self, x, y):
+        Item.__init__(self,x,y, "")
+
+    def draw(self):
+        Item.draw(self)
+
+    def update(self):
+        Item.update(self, 0)
+
+
+        
